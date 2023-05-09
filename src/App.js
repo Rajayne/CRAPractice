f62e8d53ff8caf762ce28474eeebaaf9c2ee4882
Rajayne <ryann.j.nealey@gmail.com>
2023-05-08 17:47:24 -0700

    Importing and Exporting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { sum, multiply } from "./helpers.js";
+import cats, { meow } from "./cats";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li>Name: {cats[0].name}</li>
+          <li>Midna goes {meow()}</li>
+        </ul>
+        <p>Sum(3, 7) = {sum(3, 7)}</p>
+        <p>Multiply(4, 9) = {multiply(4, 9)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/cats.js
@@ -0,0 +1,15 @@
+const cats = [
+  {
+    name: "midna",
+    age: 5,
+  },
+  {
+    name: "cricket",
+    age: 7,
+  },
+];
+
+const meow = () => "Meow!";
+
+export default cats;
+export { meow };
