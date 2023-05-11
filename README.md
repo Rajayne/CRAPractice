2fccfd4fb59e502d0502508b846f1d18f044548c
Rajayne <ryann.j.nealey@gmail.com>
2023-05-11 11:32:40 -0700

    Create handleChange and useState for form

--- README.md
+++ README.md
@@ -146,7 +146,7 @@ displayText = state (changes, can be either)
 
 - HTML form elements naturally keep some internal state and update it based on input
 - In React mutable state is kept in the state of components and only updated with the function returned to useState()
-  > Use **Controlled Components** to have React control what is shown and form state
+  > **Controlled Components** are input elements controlled by React (visible input/input state)
 
 # Testing
 
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserForm = () => {
+  const [username, setUsername] = useState("");
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+    console.log(e.target.value);
+  };
   return (
     <form>
-      <input type="text" placeholder="username"></input>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={handleChange}
+      ></input>
       <button>Add me!</button>
     </form>
   );
